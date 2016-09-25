b0d9f095d0ea11f8632ac2dc74ce383611fc879a
Yuta Watanabe <yuuta_watanabe@ricoh-japan.co.jp>
2016-09-26 00:42:21 +0900

    音声認識イベントで辞書から抽出してリスト化するように修正

--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
-$(function(){
-    var vocabs = ['じゃがいも','ジャガイモ', 'ニンジン', '人参', 'にんじん', 'たまねぎ', '玉葱', 'タマネギ'];
 
-    $('#recognizedText').on('input', function(){
-        var matched = _.filter(vocabs, function(vocab) {
-            var testRegex = new RegExp(vocab, 'i');
-            console.log(testRegex.test($('#recognizedText').val()));
-            return testRegex.test($('#recognizedText').val());
-        });
-        if(matched.length > 0){
-            $('#list').append(matched);
-            $('#recognizedText').val('');
-        }
-
+var filterVocab = function() {
+    'use strict';
+    var vocabs = ['じゃがいも','ジャガイモ', 'ニンジン', '人参', 'にんじん', 'たまねぎ', '玉ねぎ', '玉葱', 'タマネギ'];
+    return _.filter(vocabs, function(vocab) {
+        var testRegex = new RegExp(vocab, 'i');
+        console.log(testRegex.test($('#recognizedText').val()));
+        return testRegex.test($('#recognizedText').val());
     });
-});
-
-
-
-
+};
 var flag_speech = 0;
 function vr_function() {
+    'use strict';
     window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
     var recognition = new webkitSpeechRecognition();
     recognition.lang = 'ja';
@@ -42,10 +33,10 @@ function vr_function() {
     };
     //エラー
     recognition.onerror= function(event){
-        $("#recognizedText").text(event.error);
         console.log('Speech recognition error detected: ' + event.error);
         console.log('Additional information: ' + event.message);
         if(flag_speech === 0){
+            console.log('again');
             vr_function();
         }
     };
@@ -53,10 +44,17 @@ function vr_function() {
     //認識が終了したときのイベント
     recognition.onresult = function(event){
         var results = event.results;
+        var matched = filterVocab();
         for (var i = event.resultIndex; i<results.length; i++){
             //認識の最終結果
             if(results[i].isFinal){
                 $("#recognizedText").text(results[i][0].transcript);
+                if(matched.length > 0){
+                    _.each(matched, function(item) {
+                        $('#list').append('<li>' + item + '</li>');
+                    });
+                    $('#recognizedText').text('');
+                }
                 vr_function();
             }
             //認識の中間結果
@@ -71,7 +69,3 @@ function vr_function() {
     $("#state").text("開始");
     recognition.start();
 }
-
-
-
-
